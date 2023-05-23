9362293734cf92192768e5c67bc838309dfc3cff
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-23 17:07:18 -0600

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,13 @@ export class AppComponent {
   title = 'Mi tienda En Linea!';
   private firstName = "J. Alexis";
   private lastname = "Tenorio";
+  age = 82;
   completeName = this.firstName + " " + this.lastname;
-  urlImg = 'https://images.unsplash.com/photo-1684720852626-84d28dbeabc1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=798&q=80';
+  urlImg = 'https://images.unsplash.com/photo-1684720852626-84d28dbeabc1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80';
+  btnDisabled = true;
+  person = {
+    name: "Alexis",
+    age: 22,
+    avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=288&q=80"
+  }
 }
