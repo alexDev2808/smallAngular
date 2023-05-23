87e906e8b853ad272af634b7259fbc5e7322d932
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-23 16:54:38 -0600

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  // props para compartir al template deben ser publicas
+  title = 'Mi tienda En Linea!';
+  private firstName = "J. Alexis";
+  private lastname = "Tenorio";
+  completeName = this.firstName + " " + this.lastname;
+  urlImg = 'https://images.unsplash.com/photo-1684720852626-84d28dbeabc1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=798&q=80';
 }
