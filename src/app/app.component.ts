84ef694c443e491bb0f1d04effdf5c86013dfe49
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-23 19:29:42 -0600

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,14 @@ export class AppComponent {
     avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=288&q=80"
   }
 
+  vipContent = [
+    {
+      name: "Imagen VIP 01",
+      img: 'https://images.unsplash.com/photo-1627924531443-5a3261bcac81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=365&q=80'
+
+    }
+  ]
+
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
