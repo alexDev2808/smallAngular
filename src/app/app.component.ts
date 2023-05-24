dbc6548b0451fbb863d283c62d8e858a94519c2f
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-24 09:36:20 -0600

    Clases y estilos dinamicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,11 @@ export class AppComponent {
   // props para compartir al template deben ser publicas
   title = 'Mi tienda En Linea!';
   btnDisabled = true;
+  width = 10;
   person = {
     name: "Alexis",
     age: 22,
+    carrera: 'TI',
     avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=288&q=80"
   }
 
