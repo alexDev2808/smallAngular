8d741d18514f3aee16c4342c2d918c80e0fe5efc
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-23 20:01:48 -0600

    ngFor en un array de objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -25,6 +26,41 @@ export class AppComponent {
 
   nombres: string[] = ["Alexis", "Juli", "Fernanda", "Martha", "Mariela", "Ruby"];
   nuevoNombre = '';
+
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'https://images.unsplash.com/photo-1566576912321-d58ddd7a6088?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=80',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://images.unsplash.com/photo-1485965120184-e220f721d03e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=620&q=80'
+    },
+    {
+      name: 'Colleción de albums',
+      price: 34,
+      image: 'https://images.unsplash.com/photo-1587731556938-38755b4803a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YWxidW1zfGVufDB8fDB8fHww&auto=format&fit=crop&w=620&q=80'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=620&q=80'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: 'https://images.unsplash.com/photo-1520038410233-7141be7e6f97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=620&q=80'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: 'https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=620&q=80'
+    }
+  ]
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
